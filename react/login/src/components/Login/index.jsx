d2867dbd6f053cf07342f438ebcaf299da23b820
seanpolid <seanpolidori0@gmail.com>
2024-04-01 19:18:49 -0400

    Customized login page

--- react/login/src/components/Login/index.jsx
+++ react/login/src/components/Login/index.jsx
@@ -1,3 +1,5 @@
+import github from '../../assets/github-mark.svg'
+import google from '../../assets/google-icon.svg'
 import style from './style.module.css'
 import { useState } from "react"
 
@@ -15,31 +17,50 @@ const Login = () => {
 
     return (
         <section className={style.container}>
-            <form method="POST">
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        name="email" 
-                        onChange={handleEmailChange} 
-                        value={email}
-                    />
-                </div>
-                
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input 
-                        type="password" 
-                        id="password" 
-                        name="password" 
-                        onChange={handlePasswordChange}
-                        value={password}
-                    /> 
-                </div>
+            <h1>Guava</h1>
+            
+            <div className={style.options}>
+                <div className={style.oauthClients}>
+                    <p>Sign in with:</p>
+                    <a href="/oauth/authorize/google">
+                        <img src={google} className={style.google} />
+                        <span>Google</span>
+                    </a>
+                    <a href="/oauth/authorize/github">
+                        <img src={github} className={style.github} />
+                        <span>GitHub</span>
+                    </a>
+                </div>                
 
-                <input type="submit" value="Login" />
-            </form>
+                <div className={style.line}></div>
+                <p className={style.or}>or</p>
+
+                <form method="POST">
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            name="email" 
+                            onChange={handleEmailChange} 
+                            value={email}
+                        />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password" 
+                            onChange={handlePasswordChange}
+                            value={password}
+                        /> 
+                    </div>
+
+                    <input type="submit" value="Continue" />
+                </form>
+            </div>
         </section>
     )
 }
